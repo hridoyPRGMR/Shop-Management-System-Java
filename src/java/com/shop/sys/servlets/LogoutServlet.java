f33f022835e3a2106008701c8d56924b0225e6f8
hridoymia.prgmr@gmail.com <Shaon@DESKTOP-94S6H69>
2024-05-25 16:01:02 +0600

    admin will be able to cancel a specific product order

--- src/java/com/shop/sys/servlets/LogoutServlet.java
+++ src/java/com/shop/sys/servlets/LogoutServlet.java
@@ -18,7 +18,7 @@ public class LogoutServlet extends HttpServlet {
             HttpSession session=request.getSession(false);
             if(session!=null){
                 session.invalidate();
-                out.println("ok");
+                response.sendRedirect("Login.jsp");
             }
         }
     }
